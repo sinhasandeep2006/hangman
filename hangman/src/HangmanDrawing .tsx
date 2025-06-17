9a4ed890712ba32f636c75945dd5c3355ba920ed
sinha <1233sandeepsinha@gmail.com>
2025-06-17 19:34:31 +0530

    working now

--- hangman/src/HangmanDrawing .tsx
+++ hangman/src/HangmanDrawing .tsx
@@ -1,152 +1,127 @@
-import React from "react";
-import face from "./assets/FACE.jpg"
-const HEAD=(
-  <div style={{
-    height: "70px",
-          width: "70px",
-          background: "#a0785a ",
-          
-          marginLeft: "120px",
-          border: "3px solid black", // Added a solid border with color
-          borderRadius: "100%",
-          position:"absolute",
-          top:"50px",
-          right:"-30px",
-  }}></div>
-)
-const BODY=(
-  <div style={{
-    height: "100px",
-          width: "10px",
-          background: "#a0785a ",
-          
-          border: "0.2px solid black", // Added a solid border with color
-          borderRadius: "20%",
-          position:"absolute",
-          top:"123px",
-          right:0,
-          zIndex:33,
-  }}></div>
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
 )
 
-const RIGHT=(
-  <div style={{
-    height: "10px",
-          width: "100px",
-          background: "#a0785a ",
-          
-          border: "0.2px solid black", // Added a solid border with color
-          borderRadius: "20%",
-          position:"absolute",
-          top:"150px",
-          right:'-100px',
-          rotate:"-30deg",
-          transformOrigin: "left bottom"
-  }}></div>
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
 )
 
-const LEFT=(
-  <div style={{
-    height: "10px",
-          width: "100px",
-          background: "#a0785a ",
-          
-          border: "0.2px solid black", // Added a solid border with color
-          borderRadius: "20%",
-          position:"absolute",
-          top:"150px",
-          right:'10px',
-          rotate:"30deg",
-          transformOrigin: "RIGHT bottom"
-  }}></div>
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
 )
 
-const LEFT_LEG=(
-  <div style={{
-    height: "10px",
-          width: "100px",
-          background: "#a0785a ",
-          
-          border: "0.2px solid black", // Added a solid border with color
-          borderRadius: "20%",
-          position:"absolute",
-          top:"215px",
-          right:'10px',
-          rotate:"-30deg",
-          transformOrigin: "RIGHT"
-  }}></div>
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
 )
-const RIGHT_LEG=(
-  <div style={{
-    height: "10px",
-          width: "100px",
-          background: "#a0785a ",
-          
-          border: "0.2px solid black", // Added a solid border with color
-          borderRadius: "20%",
-          position:"absolute",
-          top:"265px",
-          right:'-80px',
-          rotate:"30deg",
-          transformOrigin: "RIGHT BOTTOM"
-  }}></div>
+
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
 )
-const HangmanDrawing = () => {
+
+const LEFT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: 0,
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+)
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number
+}
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
   return (
     <div style={{ position: "relative" }}>
-      {HEAD}
-      {BODY}
-      {RIGHT}
-      {LEFT}
-      {LEFT_LEG}
-      {RIGHT_LEG}
+      {BODY_PARTS.slice(0, numberOfGuesses)}
       <div
         style={{
           height: "50px",
           width: "10px",
-          background: "#a0785a ",
-          marginLeft: "120px",
-          border: "1.5px solid black", // Added a solid border with color
-          borderRadius: "50px",
-          position:"absolute",
-          top:0,
-          right:0,
-         
+          background: "black",
+          position: "absolute",
+          top: 0,
+          right: 0,
         }}
-      ></div>
+      />
       <div
         style={{
           height: "10px",
           width: "200px",
-          background: "#a0785a ",
+          background: "black",
           marginLeft: "120px",
-          border: "1.5px solid black", // Added a solid border with color
-          borderRadius: "50px",
         }}
-      ></div>
+      />
       <div
         style={{
           height: "400px",
           width: "10px",
-          background: "#a0785a ",
+          background: "black",
           marginLeft: "120px",
-          border: "1.5px solid black", // Added a solid border with color
-          borderRadius: "50px",
-          
         }}
-      ></div>
-      <div
-        style={{
-          height: "10px",
-          width: "250px",
-          background: "#a0785a ",
-          border: "1.5px solid black", // Added a solid border with color
-          borderRadius: "50px",
-          
-        }}
-      ></div>
+      />
+      <div style={{ height: "10px", width: "250px", background: "black" }} />
     </div>
-  );
-};
-
-export default HangmanDrawing;
+  )
+}
